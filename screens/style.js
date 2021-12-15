06a22dcc5637a991a1745fb8ca6628f4e58b3f55
Leonardo Pimentel Ferreira <leonardoferreira@softbox.com.br>
2021-12-15 18:57:34 -0300

    fix: toggle button  ux

--- screens/style.js
+++ screens/style.js
@@ -18,7 +18,7 @@ function getStyles(theme) {
             display: 'none'
         },
         dateLabel: { color: colors.placeholder, fontSize: 12, paddingTop: 5 },
-        fullTank: { color: colors.placeholder, paddingTop: 5, marginBottom: -10 },
+        fullTank: { color: colors.placeholder, paddingTop: 5, marginBottom: 0 },
         splitRow: {flexDirection: 'row', justifyContent:'space-between'},
         header: { height: 50, backgroundColor: Colors.tableHeaderColor },
         text: { textAlign: 'center', fontWeight: '100' },
